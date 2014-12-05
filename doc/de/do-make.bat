4388e1caa95fb15d4e99ad8f74c554d799397c53
Mathias Garbe <mail@mathias-garbe.de>
2014-12-05 12:01:08 +0100

    Adds doc/de/ latex project

---
+++ doc/de/do-make.bat
@@ -0,0 +1,3 @@
+del out\build.txt
+cmd /C do-make_impl.bat %1 > out\build.txt
+type out\build.txt
---
+++ doc/de/do-make_impl.bat
@@ -0,0 +1,3 @@
+SET PATH=C:\Program Files (x86)\MiKTeX 2.9\miktex\bin;%PATH%;C:\cygwin\bin
+
+make %1
